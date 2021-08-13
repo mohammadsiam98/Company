720189ca020f1bc559c56dc69e48e6f717f28bc4
Mohammad Al Amin Siam <bebshakori@gmail.com>
2021-08-13 15:52:46 +0600

    Blog Blade Page Implemented

---
+++ resources/views/pages/blog/post.blade.php
@@ -0,0 +1,118 @@
+@extends('Layout.mother_layout')
+@section('content')
+
+<!--Breadcrumb Area-->
+<section class="breadcrumb-area banner-2">
+    <div class="text-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 v-center">
+                    <div class="bread-inner">
+                        <div class="bread-menu">
+                            <ul>
+                                <li><a href="index-2.html">Home</a></li>
+                                <li><a href="blog-grid.html">Blog</a></li>
+                                <li><a href="#">Blog Details</a></li>
+                            </ul>
+                        </div>
+                        <div class="bread-title">
+                            <h2>Blog Single</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End Breadcrumb Area-->
+
+<!--Start Blog Details-->
+<section class="blog-page pad-tb">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="blog-header">
+                    <h1>Website – The quickest way to take your business online</h1>
+                    <div class="row mt20 mb20">
+                        <div class="col-md-8 col-9">
+                            <div class="media">
+                                <div class="user-image bdr-radius"><img src="images/user-thumb/girl.jpg" alt="girl" class="img-fluid" /></div>
+                                <div class="media-body user-info">
+                                    <h5>By John Doe</h5>
+                                    <p>January 10, 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-3">
+                            <div class="postwatch"><i class="far fa-eye"></i> 120</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="image-set"><img src="images/blog/blog-post-1.jpg" alt="blog images" class="img-fluid" /></div>
+                <div class="blog-content mt30">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the <strong>printing and typesetting industry.</strong> Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                        to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+                        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <h3 class="mt30 mb20">Website – The quickest way to take your business online</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the <strong>printing and typesetting industry.</strong> Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it
+                        to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+                        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <h3 class="mt30 mb20">So, what is a website?</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book.
+                    </p>
+                    <div class="image-set mt20 mb20"><img src="images/blog/blog-post-2.jpg" alt="blog images" class="img-fluid" /></div>
+                    <h3 class="mt30 mb20">Essential Features of a Good Website</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                        passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <h3 class="mt30 mb20">Must-Have Features for Local Business Websites</h3>
+                    <ul class="ul-list mb30">
+                        <li>A Logical Roadmap</li>
+                        <li>Crucial Business Information</li>
+                        <li>Social Media Integration</li>
+                        <li>A Mobile-Ready Version</li>
+                        <li>Heading and tagline or USP</li>
+                        <li>Testimonials and social proof</li>
+                        <li>Optimise website speed and performance</li>
+                        <li>Contact Information and Clear Navigation</li>
+                        <li>Ability to easily update title tag and meta description</li>
+                    </ul>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="image-set mt20 mb20"><img src="images/blog/blog-1.jpg" alt="blog images" class="img-fluid" /></div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="image-set mt20 mb20"><img src="images/blog/blog-2.jpg" alt="blog images" class="img-fluid" /></div>
+                        </div>
+                    </div>
+                    <h3 class="mt30 mb20">Increased Local Exposure</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                        passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <blockquote class="rd-quote">
+                        “Nunc sed mattis diam. Suspendisse mi libero, sagittis nec varius quis, pulvinar eu nisl. Nulla in accumsan orci, a varius velit. Maecenas tincidunt mauris rutrum, eleifend sem at, sollicitudin ante.”
+                    </blockquote>
+                    <h3 class="mt30 mb20">Conclusion</h3>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book.
+                    </p>
+                </div>
+            </div>
+            <!--End Blog Details-->
+        </div>
+    </div>
+</section>
+
+
+@endsection
