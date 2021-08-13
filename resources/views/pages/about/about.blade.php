45753b5d375f44198c4e7cf5917883eebda9471b
Mohammad Al Amin Siam <bebshakori@gmail.com>
2021-08-13 15:51:08 +0600

    About Blade Page Implemented

---
+++ resources/views/pages/about/about.blade.php
@@ -0,0 +1,174 @@
+@extends('Layout.mother_layout')
+@section('content')
+
+<!--Breadcrumb Area-->
+<section class="breadcrumb-area" data-background="{{asset('images/hero/bg-gradient10.jpg')}}">
+    <div class="text-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 v-center">
+                    <div class="bread-inner">
+                        <div class="bread-menu">
+                            <ul>
+                                <li><a href="index-2.html">Home</a></li>
+                                <li><a href="#">About Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="bread-title">
+                            <h2>About Company</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End Breadcrumb Area-->
+
+<!--Start About-->
+<section class="about-agency pad-tb block-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 v-center">
+                <div class="about-image">
+                    <img src="{{asset('assets/images/aboutUs.svg')}}" alt="about us" class="img-fluid" />
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="common-heading text-l ">
+                    <span>About Us</span>
+                    <h1 class="text-radius text-light text-animation bg-b">About Agency</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End About-->
+
+
+<!--Start About-->
+<section class="about-sec bg-gradient5 pad-tb">
+    <div class="container">
+        <div class="row justify-content-center text-center">
+            <div class="col-lg-10">
+                <div class="common-heading">
+                    <span>We Are Creative Agency</span>
+                    <h1 class="mb30">Why Choose <span class="text-radius text-light text-animation bg-b">Salahuddin IT</span></h1>
+                    <p>Lorem Ipsum is text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book. <span class="text-bold">Lorem Ipsum</span> is simply dummy text of the printing and typesetting industry. <span class="text-bold">Lorem Ipsum </span> is simply dummy text of the printing and typesetting industry.</p>
+
+                    <p>Lorem Ipsum is <span class="text-bold">simply dummy</span> text of the printing and typesetting industry. Lorem Ipsum has been the industry's specimen book. Lorem Ipsum is simply dummy text of the <span class="text-bold">printing</span> and typesetting industry. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End About-->
+
+
+<!--why choose-->
+<section class="why-choos-lg pad-tb">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="itm-media-object mt40 tilt-3d">
+                    <div class="media">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{asset('assets/images/icons/computers.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>Streamlined Project Management</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                    <div class="media mt40">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{asset('assets/images/icons/worker.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>A Dedicated Team of Experts</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                    <div class="media mt40">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"> <img src="{{asset('assets/images/icons/deal.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>Completion of Project in Given Time</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="itm-media-object mt40 tilt-3d">
+                    <div class="media">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{asset('assets/images/icons/computers.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>Streamlined Project Management</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                    <div class="media mt40">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"><img src="{{asset('assets/images/icons/worker.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>A Dedicated Team of Experts</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                    <div class="media mt40">
+                        <div class="img-ab- base" data-tilt data-tilt-max="20" data-tilt-speed="1000"> <img src="{{asset('assets/images/icons/deal.svg')}}" alt="icon" class="layer"></div>
+                        <div class="media-body">
+                            <h4>Completion of Project in Given Time</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet ligula nec leo elementum semper. Mauris aliquet egestas metus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End why choose-->
+
+
+<section class="why-choose pad-tb">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class="common-heading">
+                    <span>We Are Awesome</span>
+                    <h2 class="mb30">Why Choose Us</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row upset">
+            <div class="col-lg-3 col-sm-6 mt30">
+                <div class="s-block up-hor">
+                    <div class="s-card-icon"><img src="{{asset('assets/images/icons/research.svg')}}" alt="service" class="img-fluid" /></div>
+                    <h4>Reasearch and Analysis</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mt30">
+                <div class="s-block up-hor">
+                    <div class="s-card-icon"><img src="{{asset('assets/images/icons/chat.svg')}}" alt="service" class="img-fluid" /></div>
+                    <h4>Negotiation and power</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mt30">
+                <div class="s-block up-hor">
+                    <div class="s-card-icon"><img src="{{asset('assets/images/icons/monitor.svg')}}" alt="service" class="img-fluid" /></div>
+                    <h4>Creative and innovative solutions</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mt30">
+                <div class="s-block up-hor">
+                    <div class="s-card-icon"><img src="{{asset('assets/images/icons/trasparency.svg')}}" alt="service" class="img-fluid" /></div>
+                    <h4>Trasparency and ease of work</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End why-choose-->
+
+@endsection
