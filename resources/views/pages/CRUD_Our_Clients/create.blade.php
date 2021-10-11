f73d26a9c3852cb4aab99674804a94262891bd2a
Mohammad Al Amin Siam <bebshakori@gmail.com>
2021-10-11 10:07:24 +0600

    CRUD ServicesCategory Modified

--- resources/views/pages/CRUD_Our_Clients/create.blade.php
+++ resources/views/pages/CRUD_Our_Clients/create.blade.php
@@ -537,6 +537,44 @@
                         </nav>
                     </div>
 
+
+                    {{-- Instruction Modal --}}
+                    <div class="row">
+                        <div id="modalOptionalSizes" class="col-lg-12 layout-spacing">
+                            <div class="statbox widget box box-shadow">
+                                <div class="widget-content widget-content-area">
+                                    <div class="text-center">
+                                        <button type="button" class="btn btn-primary mb-2 mr-2" data-toggle="modal" data-target=".bd-example-modal-lg">Instructions</button>
+                                    </div>
+
+                                    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="myLargeModalLabel">Read The Guidelines</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                                                            <line x1="18" y1="6" x2="6" y2="18"></line>
+                                                            <line x1="6" y1="6" x2="18" y2="18"></line>
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p class="modal-text">In this Section we will upload all our client's company images. The result will show in the main homepage. Below You can see in which section does it affect in our website.</p>
+                                                    <img src="{{asset('dashboardAssets/assets/img/OurClients.PNG')}}" style=" display:block; margin-left:auto; margin-right:auto; width:300px" alt="">
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- Instruction Modal --}}
+
                     {{-- main form start --}}
                     <div class="row">
                         <div id="flHorizontalForm" class="col-lg-12 layout-spacing">
